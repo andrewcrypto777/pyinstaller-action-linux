e7c5b82d41550d5969316a45b39c60d61c1f98c1
andrewcrypto777 <151258395+andrewcrypto777@users.noreply.github.com>
2023-12-09 17:32:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,40 +41,14 @@ fi
 
 cd $WORKDIR
 
-
-if [[ $7 == "true" ]]; then
-    echo "UPDATE PACKAGE"
-    apt-get update
-    echo "UPGRADE PACKAGE"
-    apt-get upgrade -y
-
-    echo "Install Gooey Dependencies"
-    apt-get install -y libwxbase3.0-0v5 libwxgtk-media3.0-gtk3-0v5 libwxgtk-webview3.0-gtk3-0v5 \
-    libwxgtk3.0-gtk3-0v5 python3-wxgtk-media4.0 python3-wxgtk-webview4.0 python3-wxgtk4.0 \
-    libgtk-3-dev libjpeg-dev
-    # /root/.pyenv/shims/pip install gooey
-fi # [$7]
-
-if [[ $8 == "true" ]]; then
-    echo "UPDATE PACKAGE"
-    apt-get update
-    echo "UPGRADE PACKAGE"
-    apt-get upgrade -y
-
-    echo "Install Kivy Dependencies"
-    sudo apt-get install -y libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev zlib1g-dev
-    sudo apt-get install -y libgstreamer1.0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good
-    /root/.pyenv/shims/pip install pygame
-fi # [$8]
-
-sudo apt-get install -y rustc
+apt-get install -y rustc
 
 if [ -f $6 ]; then
     /root/.pyenv/shims/pip install -r $6
 fi # [ -f $6 ]
 
-sudo apt-get install -y libgirepository1.0-dev build-essential libbz2-dev libreadline-dev libssl-dev zlib1g-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libcairo2-dev
-sudo apt-get install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.1 python3-wheel python3-dev
+apt-get install -y libgirepository1.0-dev build-essential libbz2-dev libreadline-dev libssl-dev zlib1g-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libcairo2-dev
+apt-get install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.1 python3-wheel python3-dev
 /root/.pyenv/shims/pip install PyGObject
 
 /root/.pyenv/shims/pyinstaller --clean -y --dist ./dist/linux --workpath /tmp $SPEC_FILE
