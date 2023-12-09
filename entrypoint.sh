f0b2d699c3955e1972474313028ad9e9108c1a8d
andrewcrypto777 <151258395+andrewcrypto777@users.noreply.github.com>
2023-12-09 17:38:54 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,33 +21,15 @@ SPEC_FILE=${4:-*.spec}
 
 /root/.pyenv/shims/python -m pip install --upgrade pip wheel setuptools
 
-#
-# In case the user specified a custom URL for PYPI, then use
-# that one, instead of the default one.
-#
-if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
-   [[ "$PYPI_INDEX_URL" != "https://pypi.python.org/simple" ]]; then
-    # the funky looking regexp just extracts the hostname, excluding port
-    # to be used as a trusted-host.
-    mkdir -p /root/.pip
-    echo "[global]" > /root/.pip/pip.conf
-    echo "index = $PYPI_URL" >> /root/.pip/pip.conf
-    echo "index-url = $PYPI_INDEX_URL" >> /root/.pip/pip.conf
-    echo "trusted-host = $(echo $PYPI_URL | perl -pe 's|^.*?://(.*?)(:.*?)?/.*$|$1|')" >> /root/.pip/pip.conf
-
-    echo "Using custom pip.conf: "
-    cat /root/.pip/pip.conf
-fi
-
 cd $WORKDIR
 
-apt-get install -y rustc
+apt-get install -y build-essential libssl-dev libffi-dev python3-dev cargo pkg-config cargo rustc
 
 if [ -f $6 ]; then
     /root/.pyenv/shims/pip install -r $6
 fi # [ -f $6 ]
 
-apt-get install -y libgirepository1.0-dev build-essential libbz2-dev libreadline-dev libssl-dev zlib1g-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libcairo2-dev
+apt-get install -y libgirepository1.0-dev build-essential libbz2-dev libreadline-dev libssl-dev zlib1g-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libcairo2-dev binutils
 apt-get install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.1 python3-wheel python3-dev
 /root/.pyenv/shims/pip install PyGObject
 
